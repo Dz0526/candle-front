95d9973e789e886ab90f4085af9fe5f3873d8c02
Dz0526 <this@dz99.me>
2023-12-15 00:27:04 +0900

    feat: primitive game screen

---
+++ src/components/GameCandle.tsx
@@ -0,0 +1,50 @@
+import { Button } from '@chakra-ui/react';
+import { Player } from '@lottiefiles/react-lottie-player';
+import { RefObject, useRef, useState } from 'react';
+
+type Props = {
+  isLit: boolean;
+};
+
+export const GameCandle = () => {
+  const ref: RefObject<Player> = useRef<Player>();
+  const [isLit, setIsLit] = useState(false);
+  return (
+    <>
+      {isLit ? (
+        <Player
+          src={
+            'https://lottie.host/e14f1cb3-b451-48aa-9372-33b8ad641669/TSfpHpOfvn.json'
+          }
+          autoplay
+          loop
+          style={{ maxWidth: '300px', maxHeight: '300px' }}
+        ></Player>
+      ) : (
+        <Player
+          ref={ref}
+          src={
+            'https://lottie.host/6823421f-5153-4d98-ae82-d18c096e90c7/dmIye8Yuaz.json'
+          }
+          autoplay
+          loop
+          style={{ maxWidth: '300px', maxHeight: '300px' }}
+          onEvent={e => {
+            if (e == 'load') {
+              setTimeout(() => {
+                ref.current?.pause();
+              }, 1500);
+            }
+          }}
+        ></Player>
+      )}
+      <Button
+        bgColor={'lightBlue'}
+        color={'white'}
+        onClick={() => setIsLit(!isLit)}
+      >
+        {isLit ? '消す' : '灯す'}
+      </Button>
+    </>
+  );
+};
