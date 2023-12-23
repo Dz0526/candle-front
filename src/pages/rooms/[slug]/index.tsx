5a142196354107e3774d53ec64ac6aea213b692d
Dz0526 <this@dz99.me>
2023-12-23 09:08:15 +0900

    fix: sonic freq

--- src/pages/rooms/[slug]/index.tsx
+++ src/pages/rooms/[slug]/index.tsx
@@ -59,12 +59,12 @@ const Game = () => {
   const [passedTime, setPassedTime] = useState(0);
   const sonicServer = useRef(
     new SonicServer({
-      coder: new SonicCoder({ freqMin: 18000, freqMax: 19000 }),
+      coder: new SonicCoder(),
     }),
   );
   const sonicSocket = useRef(
     new SonicSocket({
-      coder: new SonicCoder({ freqMin: 18000, freqMax: 19000 }),
+      coder: new SonicCoder(),
     }),
   );
   const [pageIndex, setPageIndex] = useState(0);
